71eb88ee509ab3462c5de1ce807289207479d553
Matthias Fischer <matthias@dotnetautor.de>
2014-02-23 09:26:00 +0100

    Hinzugefügt Backgrounding
    Backgrounding demonstriert die Möglichkeiten der Verwendung
    verschiedener Background Agents auf dem Windows Phone

---
+++ 10 Backgrounding/01 Notifications/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Scheduler;
+
+namespace Notifications {
+  public partial class MainPage : PhoneApplicationPage {
+
+    IEnumerable<ScheduledNotification> _notifications;
+
+    // Constructor
+    public MainPage() {
+      InitializeComponent();
+
+    }
+
+
+    private void UpdateItemsList() {
+      _notifications = ScheduledActionService.GetActions<ScheduledNotification>();
+      var scheduledNotifications = _notifications as ScheduledNotification[] ?? _notifications.ToArray();
+      EmptyTextBlock.Visibility = scheduledNotifications.Any() ? Visibility.Collapsed : Visibility.Visible;
+      NotificationListBox.ItemsSource = scheduledNotifications; 
+    }
+
+
+    protected override void OnNavigatedTo(NavigationEventArgs e) {
+      UpdateItemsList();
+    }
+
+    private void deleteButton_Click(object sender, RoutedEventArgs e) {
+      var name = (string)((Button)sender).Tag;
+      ScheduledActionService.Remove(name);
+      UpdateItemsList();
+    }
+
+    private void ApplicationBarAddButton_Click(object sender, EventArgs e) {
+      // Navigate to the AddReminder page when the add button is clicked.
+      NavigationService.Navigate(new Uri("/AddNotification.xaml", UriKind.RelativeOrAbsolute));
+    }
+  }
+}
